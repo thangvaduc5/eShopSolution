051c8b55721192e15b11860432d18e2dc4743a98
Hoàng Minh Thắng <2021010286@sv.ufm.edu.vn>
2022-10-27 22:38:38 +0700

    configuration

---
+++ eShopSolution.Data/Configurations/ProductTranslationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductTranslationConfiguration : IEntityTypeConfiguration<ProductTranslation>
+    {
+        public void Configure(EntityTypeBuilder<ProductTranslation> builder)
+        {
+            builder.ToTable("ProductTranslations");
+            builder.HasOne(x => x.Language).WithMany(x =>x.ProductTranslations).HasForeignKey(x=>x.LanguageId);
+            builder.HasKey(x => x.ProductId);
+        }
+    }
+}
